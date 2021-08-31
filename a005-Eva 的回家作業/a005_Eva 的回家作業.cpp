5c6a3aebf4a43fc6025cacc314629b6b6764064e
LittleCodingLoser <86900193+LittleCodingLoser@users.noreply.github.com>
2021-08-31 17:08:19 +0800

    Update a005_Eva 的回家作業.cpp

--- a005-Eva 的回家作業/a005_Eva 的回家作業.cpp
+++ a005-Eva 的回家作業/a005_Eva 的回家作業.cpp
@@ -7,11 +7,11 @@ int main()
     for(int i=0;i<t;i++)
     {
         cin >> a >> b >> c >> d;
-        if((b-a)==(c-b)&&(c-b)==(d-c))
+        if((b-a)==(c-b)&&(c-b)==(d-c)) //等差
         {
             cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<d+(c-b)<<endl;
         }
-         else if((b/a)==(c/b)&&(c/b)==(d/c))
+         else if((b/a)==(c/b)&&(c/b)==(d/c)) //等比
         {
             cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<d*(c/b)<<endl;
         }
